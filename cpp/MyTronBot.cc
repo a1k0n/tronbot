32da66c425a3aac9afd9acad6624416bc0a4ff67
Andy Sloane <andy@a1k0n.net>
2010-02-12 16:05:14 -0600

    added some unused junk; this is my third submission

--- cpp/MyTronBot.cc
+++ cpp/MyTronBot.cc
@@ -39,10 +39,6 @@ template <class T> struct Map {
   T& M(position p) { return map[p.x + p.y*width]; }
   T& M(int x, int y) { return map[x + y*width]; }
 
-  int degree(position x) {
-    return 4 - M(x.next(1)) - M(x.next(2)) - M(x.next(3)) - M(x.next(4));
-  }
-
   void dump(void) {
     for(int j=0;j<height;j++) {
       for(int i=0;i<height;i++) {
@@ -77,11 +73,13 @@ struct gamestate {
   }
 };
 
-gamestate curstate;
 // }}}
 
+Map<char> M;
+gamestate curstate;
+
 // {{{ imported map update garbage from original code
-void map_update(Map<char> &M)
+void map_update()
 {
   int x, y, c;
   int map_width, map_height;
@@ -192,38 +190,142 @@ struct Components {
     }
     c.dump();
   }
+  int component(const position &p) { return c(p); }
+  int connectedarea(int component) { return csize[component]; }
+  int connectedarea(const position &p) { return csize[c(p)]; }
 };
 
 // }}}
 
-int next_move(Map<char> &M) {
-  int bestm = 0;
-  int best = 50;
+// {{{ basic geometric stuff
+int runout(position p, int dir) {
+  int r = 0;
+  while(!M(p)) { r++; p = p.next(dir); }
+  return r;
+}
+
+int degree(position x) {
+  return 4 - M(x.next(1)) - M(x.next(2)) - M(x.next(3)) - M(x.next(4));
+}
+
+int turn(int d, int n) { return 1+(d-1+n)&3; }
+// }}}
+
+struct SpaceFiller
+{
+  int area, move;
+
+  static SpaceFiller fill(position p, int max_area) {
+    SpaceFiller f;
+    f.area = _fill_area(f.move, p, max_area, 0);
+    return f;
+  }
+
+#if 0
+  static SpaceFiller greedy(position p) {
+    SpaceFiller f;
+    f.area = 0;
+    for(int m=1;m<=4;m++) {
+      int nm;
+      int a = _fill_greedy(nm, p, 1);
+      if(a > f.area) { f.area = a; f.move = nm; }
+    return f;
+  }
+#endif
+
+private:
+#if 0
+  int _fill_greedy(int &move, position p0, int majordir) {
+    // try to fill in row-major or column-major order, looking ahead one row
+    // (or column) for the largest opening
+    
+  }
+  int _find_next_greedy(position p, int dir, int majordir) {
+    //  0123456789ab
+    //  >----------- ->    ##########...
+    //---   ------       ..... 1##### 
+    // travel in dir, finding all 0->1 transitions in the next column over (by majordir)
+    // for each 0->1 transition, find the run length going -dir
+    // optimize for this_runlen + next_runlen
+    int ow = M(p.next(majordir));
+    int n = 0;
+    int besta=0, bestn=0;
+    while(!M(p.next(dir))) {
+      p = p.next(dir);
+      int nw = M(p.next(majordir));
+      if(nw && nw != ow) {
+        int a = n+runout(p.prev(dir).next(majordir), turn(dir, 2));
+        turnarounds.push_back(n);
+      }
+      n++;
+    }
+  }
+#endif
+
+  // this is exponential and sucks
+  static int _fill_area(int &move, position p0, int max_area, int lastmove) {
+//    printf("_fill_area([%d,%d], area=%d)\n", p0.x, p0.y, max_area);
+    if(max_area <= 1) { move = 0; return 1; }
+    int besta = 0, bestm = 0;
+    for(int m=1;m<=4;m++) {
+      int r = 0;
+      position p = p0;
+      if(m == lastmove) continue;
+      if(M(p.next(m))) continue;
+      while(!M(p)) { r++; M(p) = 1; p = p.next(m); } // do!
+      while(r>0) {
+        // undo! (we just ran into a wall, so undo that first...)
+        r--;
+        p = p.prev(m);
+        M(p) = 0;
+        if(r>0) {
+          // now try longest to shortest...
+          int _m;
+//          printf("%d: ", m);
+          int a = r + _fill_area(_m, p, max_area - r, m);
+          if(a >= max_area) { move = m; return a; }
+          if(a > besta) { besta = a; bestm = m; }
+        }
+      }
+    }
+    move = bestm;
+    return besta;
+  }
+};
+
+const int degreescore[] = {-10, 4, 3, 2, 1};
+int next_move() {
+//  Components cp(M);
+//  c.dump();
+//  if(cp.component(curstate.p[0]) == cp.component(curstate.p[1])) { ... }
+//  else { ... }
+
   M(curstate.p[0]) = 1;
   M(curstate.p[1]) = 1;
 
-  Components c(M);
-  c.dump();
+  Components ca(M);
+//  ca.dump();
 
+  int bestm=1, bestv=0;
   for(int m=1;m<=4;m++) {
-    position x = curstate.p[0].next(m);
-    if(M(x)) continue;
-    int d = M.degree(x);
-    if(bestm == 0 || d > best) {
-      bestm = m;
-      best = d;
-    }
+    position p = curstate.p[0].next(m);
+    if(M(p)) continue;
+    int v = ca.connectedarea(p) + degreescore[degree(p)];
+    if(v > bestv) { bestv = v; bestm = m; }
+    fprintf(stderr, "move %d: ca=%d, degree=%d, v=%d\n", m, ca.connectedarea(p), degree(p), v);
   }
-  //fprintf(stderr, "moving %d; degree=%d\n", bestm, best);
+  
+
+//  SpaceFill s(curstate.p[0], c.connectedarea(curstate.p[0])/2);
+//  return SpaceFiller::greedy(curstate.p[0]).move;
   return bestm;
 }
 
 int main() {
   setlinebuf(stdout);
-  Map<char> M;
   while (true) {
-    map_update(M);
-    printf("%d\n", next_move(M));
+    map_update();
+    printf("%d\n", next_move());
   }
   return 0;
 }
