eb2be54f4b7907d6886ac8d2f00f74f791580b6a
Andy Sloane <andy@a1k0n.net>
2010-02-12 09:55:04 -0600

    added simple O(N) connected component finder

--- cpp/MyTronBot.cc
+++ cpp/MyTronBot.cc
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <map>
 
-int map_width, map_height;
-char *map = NULL;
-#define M(p) map[(p).x+(p).y*map_width]
-
+// {{{ position
 struct position {
-  static const int dx[5], dy[5];
+  static const char dx[5], dy[5];
 
   int x,y;
   position() {}
@@ -20,16 +18,51 @@ struct position {
   position prev(int move) { return position(x-dx[move], y-dy[move]); }
 };
 
-const int position::dx[5]={0,0,1,0,-1};
-const int position::dy[5]={0,-1,0,1,0};
+const char position::dx[5]={0,0,1,0,-1};
+const char position::dy[5]={0,-1,0,1,0};
+// }}}
 
+// {{{ Map
+template <class T> struct Map {
+  T *map;
+  int width, height;
+  Map() { map = NULL; }
+  Map(int w, int h) { resize(w,h); }
+  void resize(int w, int h) {
+    width = w; height = h;
+    map = new T[w*h];
+    memset(map, 0, w*h*sizeof(T));
+  }
+  ~Map() { if(map) delete[] map; }
+  T& operator()(position p) { return map[p.x + p.y*width]; }
+  T& operator()(int x, int y) { return map[x + y*width]; }
+  T& M(position p) { return map[p.x + p.y*width]; }
+  T& M(int x, int y) { return map[x + y*width]; }
 
+  int degree(position x) {
+    return 4 - M(x.next(1)) - M(x.next(2)) - M(x.next(3)) - M(x.next(4));
+  }
+
+  void dump(void) {
+    for(int j=0;j<height;j++) {
+      for(int i=0;i<height;i++) {
+        int n = map[i+j*width];
+        if(n == 0) fprintf(stderr, "  ");
+        else fprintf(stderr, "%2d", n);
+      }
+      fprintf(stderr, "\n");
+    }
+  }
+};
+// }}}
+
+// {{{ gamestate
 struct gamestate {
   position p[2]; // position in current state
   int m[2]; // last move made
 
   // affects map using m[0], m[1]
-  gamestate move() {
+  gamestate move(Map<char> M) {
     gamestate s = *this;
     M(p[0]) = 1;
     M(p[1]) = 1;
@@ -38,42 +71,33 @@ struct gamestate {
     return s;
   }
   // undoes effect on map
-  void unmove() {
+  void unmove(Map<char> M) {
     M(p[0]) = 0;
     M(p[1]) = 0;
   }
 };
 
 gamestate curstate;
-
-gamestate try_move(gamestate r, int player, int move) {
-  // only when player 2 moves does the state actually progress...  which means you need to enter both moves.
-  gamestate s = r;
-  s.m[player] = move;
-  if(player == 1) {
-    // etc etc
-  }
-  return s;
-}
+// }}}
 
 // {{{ imported map update garbage from original code
-void map_update()
+void map_update(Map<char> &M)
 {
   int x, y, c;
+  int map_width, map_height;
   int num_items = fscanf(stdin, "%d %d\n", &map_width, &map_height);
   if (feof(stdin) || num_items < 2) {
     exit(0); // End of stream means end of game. Just exit.
   }
-  if(!map) map = new char[map_width * map_height];
-  memset(map, 0, map_width*map_height);
+  if(!M.map) M.resize(map_width, map_height);
   x = 0;
   y = 0;
-  while (y < map_height && (c = fgetc(stdin)) != EOF) {
+  while (y < M.height && (c = fgetc(stdin)) != EOF) {
     switch (c) {
     case '\r':
       break;
     case '\n':
-      if (x != map_width) {
+      if (x != M.width) {
 	fprintf(stderr, "x != width in Board_ReadFromStream\n");
 	return;
       }
@@ -81,24 +105,24 @@ void map_update()
       x = 0;
       break;
     case '#':
-      if (x >= map_width) {
+      if (x >= M.width) {
 	fprintf(stderr, "x >= width in Board_ReadFromStream\n");
 	return;
       }
-      M(position(x,y)) = 1;
+      M(x,y) = 1;
       ++x;
       break;
     case ' ':
-      if (x >= map_width) {
+      if (x >= M.width) {
 	fprintf(stderr, "x >= width in Board_ReadFromStream\n");
 	return;
       }
-      M(position(x,y)) = 0;
+      M(x,y) = 0;
       ++x;
       break;
     case '1':
     case '2':
-      if (x >= map_width) {
+      if (x >= M.width) {
 	fprintf(stderr, "x >= width in Board_ReadFromStream\n");
 	return;
       }
@@ -118,20 +142,74 @@ void map_update()
 }
 // }}}
 
-int degree(position x) {
-  return 4 - M(x.next(1)) - M(x.next(2)) - M(x.next(3)) - M(x.next(4));
-}
+// {{{ connected components algorithm
 
-int next_move() {
+struct Components {
+  Map<int> c;
+  std::map<int,int> csize;
+
+  Components(Map<char> &M): c(M.width, M.height)
+  {
+    std::map<int,int> equiv;
+    int nextclass = 1;
+    for(int j=1;j<M.height-1;j++) {
+      for(int i=1;i<M.width-1;i++) {
+        if(M(i,j)) continue; // wall
+        int cup   = c(i, j-1),
+            cleft = c(i-1, j);
+        if(cup == 0 && cleft == 0) { // new component
+          c(i,j) = nextclass++;
+        } else if(cup == cleft) { // existing component
+          c(i,j) = cup;
+        } else { // join components
+          // deprecate the higher-numbered component in favor of the lower
+          if(cleft == 0 || (cup != 0 && cup < cleft)) {
+            c(i,j) = cup;
+            if(cleft != 0) equiv[cleft] = cup;
+          } else {
+            c(i,j) = cleft;
+            if(cup != 0) equiv[cup] = cleft;
+          }
+        }
+      }
+    }
+    // now make another pass to patch up the equivalences
+    for(int j=1;j<M.height-1;j++) {
+      for(int i=1;i<M.width-1;i++) {
+        while(true) {
+          std::map<int,int>::iterator e = equiv.find(c(i,j));
+          if(e == equiv.end()) break;
+          c(i,j) = e->second;
+        }
+        csize[c(i,j)] ++;
+      }
+    }
+  }
+  void dump() {
+    std::map<int,int>::iterator i;
+    for(i=csize.begin();i!=csize.end();i++) {
+      fprintf(stderr, "area %d: %d vertices\n", i->first, i->second);
+    }
+    c.dump();
+  }
+};
+
+// }}}
+
+int next_move(Map<char> &M) {
   int bestm = 0;
   int best = 50;
   M(curstate.p[0]) = 1;
   M(curstate.p[1]) = 1;
+
+  Components c(M);
+  c.dump();
+
   for(int m=1;m<=4;m++) {
     position x = curstate.p[0].next(m);
     if(M(x)) continue;
-    int d = degree(x);
-    if(bestm == 0 || abs(d-2) < best) {
+    int d = M.degree(x);
+    if(bestm == 0 || d > best) {
       bestm = m;
       best = d;
     }
@@ -142,9 +220,12 @@ int next_move() {
 
 int main() {
   setlinebuf(stdout);
+  Map<char> M;
   while (true) {
-    map_update();
-    printf("%d\n", next_move());
+    map_update(M);
+    printf("%d\n", next_move(M));
   }
   return 0;
 }
+
+// vim: sw=2:ts=8:et:foldmethod=marker
